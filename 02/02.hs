e7ae2d14a928345be917cfd7048520ff4d1d3663
Marcel Guzik <mguzik555@gmail.com>
2019-12-03 01:21:37 +0100

    finished 02

--- 02/02.hs
+++ 02/02.hs
@@ -1,8 +1,8 @@
 import Computer
 import Control.Monad
 
-program = readProgram "input.txt"
+solution program = filter (\x -> (readMemory 0 x) == 19690720) [snd $ runProgram noun verb program | noun <- [0..99], verb <- [0..99]]
 
-run noun verb = fmap (runProgram noun verb)
-
-solution = sequenceA [run noun verb program | noun <- [0..99], verb <- [0..99]]
+main = do
+    program <- readProgram "input.txt"
+    putStrLn $ show $ solution program
